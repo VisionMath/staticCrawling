191fc6fcb2d87ba025f8ec9418734f127fb9e4de
blood337 <blood337@naver.com>
2022-08-05 06:12:44 +0900

    220805 final

--- yes24List.py
+++ yes24List.py
@@ -1,17 +1,11 @@
+import time
+import pandas as pd
+import pymysql
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
-import pymysql
-import time
-import urllib.request
-
-
-def set_chrome_driver():
-    r_driver = webdriver.Chrome(ChromeDriverManager().install())
-    return r_driver
 
 
 def getTagText(tag, by, element_path):
@@ -22,7 +16,7 @@ def getTagText(tag, by, element_path):
         return ''
 
 
-driver = set_chrome_driver()
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 url = 'http://www.yes24.com/Product/Search?domain=BOOK&query=python'
 
@@ -32,6 +26,7 @@ driver.implicitly_wait(2)
 
 stopFlag = False
 book_list = []
+json_list = []
 
 while True:
     for n in range(2, 12):
@@ -48,12 +43,13 @@ while True:
             pub = getTagText(li, By.CSS_SELECTOR, 'span.authPub.info_pub > a')
             pub_date = getTagText(li, By.CSS_SELECTOR, 'span.authPub.info_date')
             price = getTagText(li, By.CSS_SELECTOR, 'div.info_row.info_price > strong > em')
+            score = getTagText(li, By.CSS_SELECTOR, 'div.info_row.info_rating > span.rating_grade > em')
 
-            image = li.find_element(By.CSS_SELECTOR,
-                                    '#yesSchList > li > div > div.item_img > div.img_canvas > span > span > a > em > img').get_attribute(
-                'src')
+            image = li.find_element(By.CSS_SELECTOR, '#yesSchList > li > div > div.item_img > div.img_canvas > span > span > a > em > img').get_attribute('src')
             image_name = image.split('/')[-2]
-            urllib.request.urlretrieve(image, 'book_images/' + image_name + '.jpg')
+            # urllib.request.urlretrieve(image, 'book_images/' + image_name + '.jpg')
+            json_list.append({'title': title, 'author': author, 'publisher': pub, 'pub_date': pub_date, 'price': price, 'score': score, 'image_src': image, 'image_name': image_name})
+            book_list.append((title, author, pub, pub_date, price, score, image, image_name))
 
         book_count = driver.find_elements(By.CLASS_NAME, 'itemUnit')
 
@@ -66,12 +62,44 @@ while True:
                 num = n + 2
             except NoSuchElementException:
                 num = n
-            page = driver.find_element(By.CSS_SELECTOR,
-                                       '#goodsListWrap > div.sGoodsPagen > div > a:nth-child(' + str(num) + ')')
+            page = driver.find_element(By.CSS_SELECTOR, '#goodsListWrap > div.sGoodsPagen > div > a:nth-child(' + str(num) + ')')
             page.click()
             time.sleep(2)
 
     if stopFlag:
         break
+df = pd.DataFrame(book_list, columns=['title', 'author', 'publisher', 'pub_date', 'price', 'score', 'image_src', 'image_name'])
+df.to_csv('data/yes24List.csv')
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+df.fillna("", inplace=True)
+book_list = df.values.tolist()
+print(book_list)
+
+
+def insert_books(books):
+    conn = pymysql.connect(host='localhost', user='root', password='vm28283', db='pydb', charset='utf8')
+    cursor = conn.cursor()
+    sql = 'insert into yes24List(title, author, publisher, pub_date, price, score, image_src, image_name) values(%s, %s, %s, %s, %s, %s, %s, %s)'
+    cursor.executemany(sql, books)
+    conn.commit()
+    conn.close()
+
+
+def select_all():
+    conn = pymysql.connect(host='localhost', user='root', password='vm28283', db='pydb', charset='utf8')
+    cursor = conn.cursor()
+    sql = 'select * from yes24List'
+    cursor.execute(sql)
+    for book in cursor:
+        print(book)
+    conn.commit()
+    conn.close()
+
+
+insert_books(book_list)
+select_all()
 
 driver.quit()
